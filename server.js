65bf56d6099a3f967aa2ba32844742cba6412aa1
AmanGupta445052 <2105351@kiit.ac.in>
2024-08-17 03:20:57 +0530

    port updated with enviroment variable

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 
 app.use("/public", express.static(__dirname + "/public"))
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("The server is up and running in 3000!");
 }); 
